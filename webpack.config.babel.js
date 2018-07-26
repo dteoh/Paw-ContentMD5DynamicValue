47674c306664bd875781088ebc62c4aae341adce
Douglas Teoh <douglas@dteoh.com>
2018-07-26 10:12:37 +0900

    Initial setup
    Based on luckymarmot/Paw-DigestAuthDynamicValue repo

---
+++ webpack.config.babel.js
@@ -0,0 +1,33 @@
+import webpack from 'webpack'
+import path from 'path'
+
+const name = 'ContentMD5DynamicValue'
+
+const production = process.env.NODE_ENV === 'production'
+
+const config = {
+    target: 'web',
+    entry: [
+        'immutable',
+        './src/ContentMD5DynamicValue.js'
+    ],
+    output: {
+        path: path.join(__dirname,
+            './build/com.dteoh.PawExtensions.ContentMD5DynamicValue'),
+        pathInfo: true,
+        publicPath: '/build/',
+        filename: name + '.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                include: [
+                    path.resolve(__dirname, 'src'),
+                ],
+                test: /\.jsx?$/,
+            }
+        ]
+    }
+}
+module.exports = config
